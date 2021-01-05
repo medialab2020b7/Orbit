a2d2a44f21f054e49dc8f671eb43ed6bda818d9c
Madalena Sousa <madalenaantunessousa@gmail.com>
2021-01-05 20:09:24 +0000

    css perfil

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,7 +2,7 @@
 
 @section('head')
     <style>
-        .main {
+        main {
             overflow-x: hidden;
         }
 
@@ -66,6 +66,22 @@
             background-color: transparent;
             color: black;
         }
+
+        h1 {
+            -webkit-text-stroke-width: 1px;
+            -webkit-text-stroke-color: red;
+            color: transparent;
+        }
+
+        h2 {
+            -webkit-text-stroke-width: 1px;
+            -webkit-text-stroke-color: blue;
+            color: transparent;
+        }
+
+        h3 {
+            font-family: "Monument Extended", "Helvetica LT Ext", sans-serif;
+        }
     </style>
 @endsection
 
@@ -104,21 +120,21 @@
                     </div>
                 </div>
                 <div class="px-4">
-                    <h2 class="mb-0">Localization</h2>
+                    <h2 class="mb-0">LOCATION</h2>
                     <div class="p-4 rounded shadow-sm">
                         <p>Country: {{ $country }}</p>
                         <p>City: {{ $city }}</p>
                     </div>
                 </div>
                 <div class="px-4">
-                    <h2 class="mb-0">About</h2>
+                    <h2 class="mb-0">ABOUT</h2>
                     <div class="p-4 rounded shadow-sm">
                         <p>{{ $user->description }}</p>
                     </div>
                 </div>
                 <div class="px-4">
                     <div class="d-flex align-items-center justify-content-between mb-3">
-                        <h2 class="mb-0">Stories</h2><a href="#" class="btn btn-link text-muted">Show all</a>
+                        <h2 class="mb-0">STORIES</h2><a href="#" class="btn btn-link text-muted">Show all</a>
                     </div>
                     <div class="row">
                         <!-- Stories List -->
@@ -128,9 +144,9 @@
                                     <a href="#" class="story list-group-item list-group-item-action flex-column align-items-start" data-toggle="modal"
                                        data-target="#profileStoryModal" data-id={{$h->id}}>
                                         <div class="d-flex w-100 justify-content-between">
-                                            <h2 class="mb-1">
+                                            <h3 class="mb-1">
                                                 {{ $h->emotion->name ?? 'emotion_not_defined' }}
-                                            </h2>
+                                            </h3>
                                             <small>{{ $h->history_date }}</small>
                                         </div>
                                         <p class="mb-1">{{ $h->description }}</p>
@@ -148,7 +164,7 @@
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h1 class="modal-title" id="showStory"></h1>
+                                <h3 class="modal-title" id="showStory"></h3>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
